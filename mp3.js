62e59a9c06c8e07ebd494d00492cf0a934813a05
otmb <m.otsu77@gmail.com>
2024-11-04 20:04:51 +0900

    mp3.js refactoring

--- mp3.js
+++ mp3.js
@@ -1,34 +1,24 @@
-let log = function(txt){
-  console.log(txt)
-}
-
 async function buildMp3(audioBuffer, bitrate, vbr) {
   
-  let ch=audioBuffer.numberOfChannels;
-  let x;
   message.innerText = "Browser Storage Writing";
-  if(ch==1){
-      x = audioBuffer.getChannelData(0);
-      this.writeFile(x, "audioBufferL");
+  if(audioBuffer.numberOfChannels==1){
+    writeFile("audioBufferL", audioBuffer.getChannelData(0));
   } else {
-      const l=audioBuffer.getChannelData(0),r=audioBuffer.getChannelData(1);
-      this.writeFile(l, "audioBufferL");
-      this.writeFile(r, "audioBufferR");
+    writeFile("audioBufferL", audioBuffer.getChannelData(0));
+    writeFile("audioBufferR", audioBuffer.getChannelData(1));
   }
-  const numberOfChannels = audioBuffer.numberOfChannels,
-            sampleRate = audioBuffer.sampleRate,
-            nsamples = audioBuffer.length;
   message.innerText = "Start Encode";
-  const ret = await Instance.encode(sampleRate, numberOfChannels, bitrate, vbr, nsamples);
+  const ret = await Instance.encode(
+                      audioBuffer.sampleRate, 
+                      audioBuffer.numberOfChannels, 
+                      bitrate, vbr, audioBuffer.length);
   console.log("Result: ", ret);
   message.innerText = "Complete";
   if (ret > 0){
-    let mp3 = Module.FS.readFile("output.mp3");
-    return mp3;
+    return Module.FS.readFile("output.mp3");
   }
   return;
 }
-async function writeFile(x, name){
-  let uint8 = new Uint8Array(x.buffer);
-  Module.FS.writeFile(name, uint8);
+function writeFile(name,x){
+  Module.FS.writeFile(name, new Uint8Array(x.buffer));
 }
