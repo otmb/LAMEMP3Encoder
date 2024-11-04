8731700da86f7bfa5b8f16f414af2bab3978ecd8
otmb <m.otsu77@gmail.com>
2024-11-04 20:11:00 +0900

    mp3.js refactoring

--- mp3.js
+++ mp3.js
@@ -1,10 +1,8 @@
 async function buildMp3(audioBuffer, bitrate, vbr) {
   
   message.innerText = "Browser Storage Writing";
-  if(audioBuffer.numberOfChannels==1){
-    writeFile("audioBufferL", audioBuffer.getChannelData(0));
-  } else {
-    writeFile("audioBufferL", audioBuffer.getChannelData(0));
+  writeFile("audioBufferL", audioBuffer.getChannelData(0));
+  if(audioBuffer.numberOfChannels==2){
     writeFile("audioBufferR", audioBuffer.getChannelData(1));
   }
   message.innerText = "Start Encode";
